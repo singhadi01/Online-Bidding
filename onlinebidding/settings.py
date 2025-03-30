da77da88e3c095ce98a7214f3d0333ee4fcb3f7a
Aditya Singh <140547416+singhadi01@users.noreply.github.com>
2025-03-30 17:03:58 +0530

    Update settings.py

--- onlinebidding/settings.py
+++ onlinebidding/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0fqewf55ruk+#vr=vrfm2pjd3mtku!503o@6%v0_pjl+brr9$8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
